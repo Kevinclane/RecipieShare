d47ac33a0a480905440ea25600a10f7e1f7d35e8
Kevinclane <kevinclane21@gmail.com>
2020-10-05 15:17:30 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+let base = window.location.host.includes("localhost")
+  ? "//localhost:3000/"
+  : "/";
+
+let api = Axios.create({
+  baseURL: base + "api/",
+  timeout: 3000,
+  withCredentials: true,
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { apiResponse: "" }
+  }
+
+  async callAPI() {
+    try {
+      let res = await api.get("userinfo")
+      this.state.apiResponse = res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  componentWillMount() {
+    this.callAPI()
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.apiResponse}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
