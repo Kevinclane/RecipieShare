31fd73c90a4da0719d8c2e2f9bfceae250626de0
Kevinclane <kevinclane21@gmail.com>
2020-10-22 13:53:52 -0600

    start

--- src/Components/Addition.js
+++ src/Components/Addition.js
@@ -10,12 +10,13 @@ class Addition extends React.Component {
       num1: 0,
       num2: 0,
       difficulty: "",
-      difficultySelect: true
+      difficultySelect: true,
+      userAnswer: ""
     }
   }
-  componentDidMount() {
-    this.generateEasyProblem()
-  }
+  // componentDidMount() {
+  //   this.generateEasyProblem()
+  // }
 
   render() {
     if (this.state.difficultySelect) {
@@ -30,11 +31,27 @@ class Addition extends React.Component {
             <i className="fa fa-arrow-left fa-3x cursor" aria-hidden="true" onClick={() => this.props.changeView("home")}></i>
           </div>
           <div className="row">
-            <div className="col">
-              num1: {this.state.num1}
-            num2: {this.state.num2}
+            <div className="col text-center number-text">
+              {this.state.num1}
+            </div>
+            <div className="col text-center number-text">
+              +
+            </div>
+            <div className="col text-center number-text">
+              {this.state.num2}
             </div>
           </div>
+          <div className="row">
+            <div className="col text-center">
+              <h1>Type your answer below</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col text-center ">
+              <input className="answer-form" onKeyPress={this.checkAnswer} onChange={this.updateInput} value={this.state.userAnswer} />
+            </div>
+          </div>
+          {this.state.userAnswer}
         </div>
       )
     }
@@ -46,10 +63,43 @@ class Addition extends React.Component {
       num2: Math.ceil(Math.random() * 10)
     })
   }
+  generateMediumProblem = () => {
+    this.setState({
+      num1: Math.ceil(Math.random() * 30),
+      num2: Math.ceil(Math.random() * 30)
+    })
+  }
+  generateHardProblem = () => {
+    this.setState({
+      num1: Math.ceil(Math.random() * 1000),
+      num2: Math.ceil(Math.random() * 1000)
+    })
+  }
+  checkAnswer = (e) => {
+    if (e.key === "Enter") {
+      const answer = parseInt(this.state.userAnswer)
+      if (answer === this.state.num1 + this.state.num2) {
+
+      } else { console.log("wrong") }
+    }
+  }
+  updateInput = (e) => {
+    this.setState({
+      userAnswer: e.target.value
+    })
+  }
   chooseDifficulty = (choice) => {
     this.setState({
-      difficulty: choice
+      difficulty: choice,
+      difficultySelect: false
     })
+    if (choice === "easy") {
+      this.generateEasyProblem()
+    } else if (choice === "medium") {
+      this.generateMediumProblem()
+    } else {
+      this.generateHardProblem()
+    }
   }
 }
 export default Addition
--- src/Components/Difficulty.js
+++ src/Components/Difficulty.js
@@ -10,16 +10,16 @@ class Difficulty extends React.Component {
       <div className="text-center my-2 container">
         <h1>Choose your difficulty</h1>
         <div className="d-flex justify-content-around row my-3">
-          <span className="text-center col" onClick={() => this.props.chooseDifficulty("easy")}>
+          <span className="text-center col cursor" onClick={() => this.props.chooseDifficulty("easy")}>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
             <h4 className="my-3">Easy</h4>
           </span>
-          <span className="text-center col" onClick={() => this.props.chooseDifficulty("medium")}>
+          <span className="text-center col cursor" onClick={() => this.props.chooseDifficulty("medium")}>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
             <h4 className="my-3">Medium</h4>
           </span>
-          <span className="text-center col" onClick={() => this.props.chooseDifficulty("hard")}>
+          <span className="text-center col cursor" onClick={() => this.props.chooseDifficulty("hard")}>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
             <i className="fa fa-star fa-4x text-gold" aria-hidden="true"></i>
